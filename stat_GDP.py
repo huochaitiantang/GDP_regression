f8bb8cfd1fe02105817fe0b8420dad7da5d0f51c
liuliang <1432249204@qq.com>
2018-11-27 11:48:02 +0800

    refractor regression class & add backward model

--- stat_GDP.py
+++ stat_GDP.py
@@ -53,11 +53,13 @@ class ParseFile:
 # Class of linear regression model
 class RegressionModel:
 
-    def __init__(self, X, Y):
+    def __init__(self, X, Y, X_names, Y_name):
         self._X = X
         self._Y = Y
         self._n = Y.shape[0]
         self._p = X.shape[1] - 1
+        self._X_names = X_names
+        self._Y_name = Y_name
 
         self._XTX_inv = None
         self._est_beta = None
@@ -69,6 +71,7 @@ class RegressionModel:
         self._F = None
 
         self._abs_Ts = None
+        self._Fs = None
 
     # Calculate the Estimate Beta by the Least Squares Approximation
     def cal_est_beta(self):
@@ -102,18 +105,22 @@ class RegressionModel:
 
     # Check if beta[i] == 0
     # T[i] = est_beta[i] / (sqrt(Q / (n - p - 1)) * sqrt(XTX_inv[i][i]))
+    # F[i] = est_beta[i]^2 / ((Q / (n - p - 1)) * XTX_inv[i][i])
     # T[i] ~ t(n - p - 1)
-    def cal_abs_Ts(self):
+    # F[i] ~ F(1, n - p - 1)
+    def cal_abs_Ts_Fs(self):
         cigma = np.sqrt(self._Q / (self._n - self._p - 1.))
         self._abs_Ts = [0., ]
+        self._Fs = [0., ]
         for i in range(1, self._p + 1):
             t = np.abs(self._est_beta[i, 0] / (cigma * np.sqrt(self._XTX_inv[i,i])))
             self._abs_Ts.append(t)
+            self._Fs.append(t * t)
     
     def solve(self):
         self.cal_est_beta()
         self.cal_metric()
-        self.cal_abs_Ts()
+        self.cal_abs_Ts_Fs()
 
     def X(self):
         return self._X
@@ -148,57 +155,120 @@ class RegressionModel:
     def abs_Ts(self):
         return self._abs_Ts
 
+    def Fs(self):
+        return self._Fs
 
-def format_result(parseFile, model):
-    X_names = parseFile.X_names()
-    beta = model.est_beta()
-    abs_Ts = model.abs_Ts()
+    def format_result(self):
+        print("n = {}, p = {}, n - p - 1 = {}".format(self._n, self._p, self._n - self._p - 1))
+        print("Y: {}".format(self._Y_name))     
+    
+        print("-------------------------------Solve Result----------------------------------")
+        print("Xi\t| EstBeta\t| T-Value\t| F-Value\t| X_name")
+        print("-----------------------------------------------------------------------------")
+    
+        for i in range(self._p + 1):
+            print("X{}\t| {:.5f}\t| {:.5f}\t| {:.5f}\t| {}".format(i, self._est_beta[i,0], self._abs_Ts[i], self._Fs[i], self._X_names[i]))
+        print("-----------------------------------------------------------------------------")
+    
+        print("\tQ:  {:.5f}".format(self._Q))
+        print("\tU:  {:.5f}".format(self._U))
+        print("\tF:  {:.5f}".format(self._F))
+        print("\tR2: {:.5f}".format(self._R2))
+        print("\tmean-Q:  {:.5f}".format(self._Q / self._n))
+        print("-----------------------------------------------------------------------------")
 
-    print("n = {}, p = {}, n - p - 1 = {}".format(model.n(), model.p(), model.n() - model.p() - 1))
-    print("Y: {}".format(parseFile.Y_name()))     
+    def visual(self, start_number):
+        # plot the estimate result
+        x = np.arange(start_number, start_number + self._n)
+        true_y = self._Y[:,0][::-1]
+        est_y  = self._est_Y[:,0][::-1]
+    
+        plt.title('Estimated and True GDP')
+        plt.xlabel('Year')
+        plt.ylabel('GDP(100 million)')
+    
+        plt.plot(x, true_y, "r^-", label="true")
+        plt.plot(x, est_y, "cv:", label="estimated")
+        plt.legend(loc='upper left')
+    
+        plt.show()
 
-    print("---------------------Solve Result--------------------------")
-    print("Xi\t| EstBeta\t| T-Value\t| X_name")
-    print("-----------------------------------------------------------")
-
-    for i in range(model.p() + 1):
-        print("X{}\t| {:.5f}\t| {:.5f}\t| {}".format(i, beta[i,0], abs_Ts[i], X_names[i]))
-    print("-----------------------------------------------------------")
-
-    print("Q:  {:.5f}".format(model.Q()))
-    print("U:  {:.5f}".format(model.U()))
-    print("F:  {:.5f}".format(model.F()))
-    print("R2: {:.5f}".format(model.R2()))
-    print("mean-Q:  {:.5f}".format(model.Q() / model.n()))
 
 
-def visual(model, start_number):
-    # plot the estimate result
-    x = np.arange(start_number, start_number + model.n())
-    true_y = model.Y()[:,0][::-1]
-    est_y  = model.est_Y()[:,0][::-1]
+# Backward method for variable choice
+class BackwardRegressionModel:
+    
+    def __init__(self, X, Y, X_names, Y_name, F_out_thresh):
+        self._X = X
+        self._Y = Y
+        self._X_names = X_names
+        self._Y_name = Y_name
+        self._F_out = F_out_thresh
+        # Variance abandoned
+        self._pop = []
 
-    plt.title('Estimated and True GDP')
-    plt.xlabel('Year')
-    plt.ylabel('GDP(100 million)')
+    # filter the pop variance xi, get the reset feature
+    def filter_X(self):
+        fX = self._X.copy()
+        if len(self._pop) > 0:
+            fX = np.delete(fX, self._pop, axis=1)
+        fX_names = []
+        maps = []
+        for i in range(len(self._X_names)):
+            if i not in self._pop:
+                fX_names.append(self._X_names[i])
+                maps.append(i)
+        return fX, fX_names, maps
 
-    plt.plot(x, true_y, "r^-", label="true")
-    plt.plot(x, est_y, "cv:", label="estimated")
-    plt.legend(loc='upper left')
+    def solve(self):
+        step = 0
+        while True:
+            fX, fX_names, maps = self.filter_X()
+            #print(fX.shape, fX_names, self._pop)
+            model = RegressionModel(fX, self._Y, fX_names, self._Y_name)
+            model.solve()
+            model.format_result()
+            Fs = model.Fs()
+            min_F_value = np.min(Fs[1:])
+            min_F_index = np.argmin(Fs[1:]) + 1
+            if min_F_value >= self._F_out:
+                break
+            origin_index = maps[min_F_index]
+            self._pop.append(origin_index)
+            self._pop.sort()
+            step += 1
+            print("\n* Step [ {} ]: Remove X{}: {} Fi = {:.5f} < {}".format(step, origin_index, self._X_names[origin_index], min_F_value, self._F_out))
+        est_beta = model.est_beta()[:,0]
+        abs_Ts = model.abs_Ts()
+        Fs = model.Fs()
+        print("---------------------------Backward Result-----------------------------------")
+        print("Xi\t| Est-Beta\t| T-Value\t| F-Value\t| X_name")
+        print("-----------------------------------------------------------------------------")
+        for i in range(est_beta.shape[0]):
+            print("X{}\t| {:.5f}\t| {:.5f}\t| {:.5f}\t| {}".format(maps[i], est_beta[i], abs_Ts[i], Fs[i], self._X_names[maps[i]]))
+        print("-----------------------------Backward End------------------------------------")
+        model.visual(1978)
 
-    plt.show()
 
 
 def main():
     #parseFile = ParseFile("linear.csv")
     parseFile = ParseFile("data_GDP.csv")
 
-    model = RegressionModel(parseFile.X(), parseFile.Y())
+    # All model
+    '''
+    model = RegressionModel(parseFile.X(), parseFile.Y(), parseFile.X_names(), parseFile.Y_name())
     model.solve()
+    model.format_result()
+    model.visual(1978)
+    '''
 
-    format_result(parseFile, model)
-    visual(model, 1978)
-
+    # Backward choose model
+    # alpha = 0.05, F(1, 23) = 4.28
+    # alpha = 0.01, F(1, 23) = 7.88
+    F_out = 7.88
+    model = BackwardRegressionModel(parseFile.X(), parseFile.Y(), parseFile.X_names(), parseFile.Y_name(), F_out)
+    model.solve()
 
 
 if __name__ == "__main__":
